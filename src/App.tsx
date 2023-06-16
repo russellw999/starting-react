0aaf8bc71a5abe60f8a5a2105d0210adcf772c21
russellw999 <russellw999@gmail.com>
2023-06-16 19:22:39 +0200

    02 Introductiuon to React

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div
+        style={{
+          margin: "auto",
+          width: 800,
+          paddingTop: "1rem",
+        }}
+      >
+        <h1 className="title">Pokemon Search</h1>
+        <table width="100%">
+          <thead>
+          <tr>
+              <th>Name</th>
+              <th>Type</th>
+            </tr>         
+          </thead>
+          <tbody>
+            <tr>
+              <td>Balbasaur</td>
+              <td>Grass, Poison</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
 export default App
